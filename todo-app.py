cf65dc4b9bcbbfeab2e3e74a6d94a37838c5dd11
Matheus Saunders <matheussaunders7@gmail.com>
2026-02-19 21:42:59 -0300

    Implementando editar tarefa e alternar concluida (CRUD completo)

--- todo-app.py
+++ todo-app.py
@@ -9,7 +9,7 @@ def carregar_tarefas():
         with open("tarefas.json", "r") as arquivo:
             return json.load(arquivo) 
     except: #caso nao tenha nada na lista ele retorna ela vazia
-        return []
+        return FileNotFoundError
 
 tarefas = carregar_tarefas()
 
@@ -27,8 +27,10 @@ def menu():
     print("""
 [1]Adicionar tarefa
 [2]Ver Tarefas
-[3]Marcar tarefas como concluida
-[4]sair
+[3]Alternar concluida (marcar/desmarcar)
+[4]Excluir tarefa
+[5]Editar tarefa
+[6]Sair
 """)
 
 def adicionar_tarefa(tarefas):
@@ -51,19 +53,48 @@ def listar_tarefas(tarefas):
             status = "[ ]"
         print(f"{i} tarefa: {tarefa['tarefas']} - {status}")
 
-def marcar_concluida(tarefas):
+def excluir_tarefa(tarefas):
     if not tarefas:
         print("voce ainda nao tem nenhuma tarefa")
         return
     listar_tarefas(tarefas)
-    valor = ler_int("informe o numero da tarefa que voce deseja marcar como concluida: ")
-    while valor < 1 or valor > len(tarefas):
-        print("voce digitou um numero invalido")
-        valor = ler_int("informe o numero da tarefa que voce deseja marcar como concluida: ")
+    valor = ler_int("informe qual das tarefas voce deseja remover: ")
+    while valor < 1 or valor > len(tarefas): #isso verifica se o usuario informou uma tarefa existente
+        print("essa tarefa nao existe")
+        valor = ler_int("informe uma tarefas valida: ")
     indice = valor - 1
-    tarefas[indice]["concluido"] = True
-    print(f"A {tarefas[indice]['tarefas']} foi marcada como concluida!")
+    removida = tarefas.pop(indice) #isso remove a tarefa 
+    print(f"A tarefa {removida['tarefas']} foi removida!")
     salvar_tarefas(tarefas) #isso salva oq esta acontecendo no arquivo json
+
+def alternar_concluida(tarefas):
+    if not tarefas:
+        print("voce ainda nao tem nenhuma tarefa")
+        return
+    listar_tarefas(tarefas)
+    valor = ler_int("informe a tarefa que voce deseja alterar: ")
+    while valor < 1 or valor > len(tarefas):
+        print("essa tarefa nao existe")
+        valor = ler_int("informe uma tarefa valida: ")
+    indice = valor - 1
+    tarefas[indice]["concluido"] = not tarefas[indice]["concluido"]
+    print(f"agora a tarefa {tarefas[indice]['tarefas']} foi alterada")
+    salvar_tarefas(tarefas)
+
+def editar_tarefas(tarefas):
+    if not tarefas:
+        print("voce ainda nao tem nenhuma tarefa")
+        return
+    listar_tarefas(tarefas)
+    valor = ler_int("informe a tarefa que voce deseja editar: ")
+    while valor < 1 or valor > len(tarefas):
+        print("essa tarefa nao existe")
+        valor = ler_int("informe uma tarefa valida: ")
+    indice = valor - 1
+    editar = input("informe o novo texto: ")
+    tarefas[indice]["tarefas"] = editar
+    print("Tarefa editada com sucesso")
+    salvar_tarefas(tarefas)
 
 while True:
     menu()
@@ -75,6 +106,10 @@ while True:
     elif escolha == 2:
         listar_tarefas(tarefas)
     elif escolha == 3:
-        marcar_concluida(tarefas)
+        alternar_concluida(tarefas)
     elif escolha == 4:
+        excluir_tarefa(tarefas)
+    elif escolha == 5:
+        editar_tarefas(tarefas)
+    elif escolha == 6:
         break
